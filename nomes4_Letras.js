75785133253f3fff0fe36f313c63dc91262f0c8d
Luiz Felipe <luizfelipe1342@yahoo.com.br>
2020-04-11 21:26:22 -0300

    Add files via upload

---
+++ nomes4_Letras.js
@@ -0,0 +1,8 @@
+var pessoas = ["João", "José", "Maria", "Sebastião", "Antônio"];
+
+for(i = 0; i <= 4; i++){
+  if(pessoas[i].length == 4){
+    console.log(pessoas[i]);
+  }
+} 
+
---
+++ somaDeGauss.js
@@ -0,0 +1,7 @@
+var soma = 0;
+
+for(i=0; i<=100; i++){
+  soma += i;
+}
+
+console.log(soma);
